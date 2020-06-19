35765a853ed975050b8ccc73818f12fbbe3c9adf
mac <mac@maxs-MacBook-Pro.local>
2020-06-19 17:44:54 +0300

    acount

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -127,6 +127,76 @@ $(document).ready(function () {
         max: 100000,
     });
     $('select').styler();
+
+    if ($('main').hasClass('card')) {
+        $('.card_slider_lg').slick({
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            arrows: false
+
+        });
+        $('.card_slider_sm').slick({
+            slidesToShow: 6,
+            slidesToScroll: 1,
+            vertical: true,
+            asNavFor: '.card_slider_lg',
+            focusOnSelect: true,
+            arrows: false
+        });
+    }
+
+    // 
+    if ($('main').hasClass('catalog')) {
+        if ($(window).width() > 767.9) {
+            (function () {
+                var a = document.querySelector('.catalog_sidebar'), b = null, P = 220;  // если ноль заменить на число, то блок будет прилипать до того, как верхний край окна браузера дойдёт до верхнего края элемента. Может быть отрицательным числом
+                window.addEventListener('scroll', Ascroll, false);
+                document.body.addEventListener('scroll', Ascroll, false);
+                function Ascroll() {
+                    if (b == null) {
+                        var Sa = getComputedStyle(a, ''), s = '';
+                        for (var i = 0; i < Sa.length; i++) {
+                            if (Sa[i].indexOf('overflow') == 0 || Sa[i].indexOf('padding') == 0 || Sa[i].indexOf('border') == 0 || Sa[i].indexOf('outline') == 0 || Sa[i].indexOf('box-shadow') == 0 || Sa[i].indexOf('background') == 0) {
+                                s += Sa[i] + ': ' + Sa.getPropertyValue(Sa[i]) + '; '
+                            }
+                        }
+                        b = document.createElement('div');
+                        b.style.cssText = s + ' box-sizing: border-box; width: ' + a.offsetWidth + 'px;';
+                        a.insertBefore(b, a.firstChild);
+                        var l = a.childNodes.length;
+                        for (var i = 1; i < l; i++) {
+                            b.appendChild(a.childNodes[1]);
+                        }
+                        a.style.height = b.getBoundingClientRect().height + 'px';
+                        a.style.padding = '0';
+                        a.style.border = '0';
+                    }
+                    var Ra = a.getBoundingClientRect(),
+                        R = Math.round(Ra.top + b.getBoundingClientRect().height - document.querySelector('.some_new').getBoundingClientRect().top + 30);  // селектор блока, при достижении верхнего края которого нужно открепить прилипающий элемент;  Math.round() только для IE; если ноль заменить на число, то блок будет прилипать до того, как нижний край элемента дойдёт до футера
+                    if ((Ra.top - P) <= 0) {
+                        if ((Ra.top - P) <= R) {
+                            b.className = 'stop';
+                            b.style.top = - R + 'px';
+                        } else {
+                            b.className = 'sticky';
+                            b.style.top = P + 'px';
+                        }
+                    } else {
+                        b.className = '';
+                        b.style.top = '';
+                    }
+                    window.addEventListener('resize', function () {
+                        a.children[0].style.width = getComputedStyle(a, '').width
+                    }, false);
+                }
+            })()
+        }
+    }
+    $('.your_order_his_btn').click(function () {
+        $(this).toggleClass('btn_change')
+        $(this).parents('.your_order').find('.your_order_delivered').slideToggle(300).css("display", "flex");
+        $(this).parents('.your_order').find('.your_order_delivery_details').slideToggle(300);
+    });
 });
 $(document).scroll(function () {
     if ($(this).scrollTop() >= 50) {
@@ -135,4 +205,6 @@ $(document).scroll(function () {
     else {
         $('header').removeClass('header_op');
     }
+
 });
+
